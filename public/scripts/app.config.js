5e42c1eb3931943f5d0bb8630baf0f77c889dfba
erik@ejnelson.com <christamichels@Christas-MacBook-Pro.local>
2017-03-01 14:22:40 -0600

    set up ngroutes, started div layout for header, created controllers

---
+++ public/scripts/app.config.js
@@ -0,0 +1,20 @@
+var app=angular.module('AngelApp',['ngRoute']);
+
+
+app.config(function($routeProvider,$locationProvider){
+  $routeProvider.when('/',{
+    templateUrl:'views/standard-report.html',
+    controller: 'StandardReportController as standardCtrl'
+  }).when('/data-upload',{
+    templateUrl:'views/data-upload.html',
+    controller: 'UploadReportController as uploadCtrl'
+  }).when('/data-vis',{
+    templateUrl:'views/data-vis.html',
+    controller: 'DataVisController as visCtrl'
+  }).when('/custom-report',{
+    templateUrl:'views/custom-report.html',
+    controller: 'CustomReportController as customCtrl'
+  });
+
+    $locationProvider.html5Mode(true);
+});
