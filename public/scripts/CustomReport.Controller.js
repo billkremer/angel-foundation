6ba53f6e3de91dccc8e4eb78bdbb629effd0de6c
erik@ejnelson.com <christamichels@Christas-MacBook-Pro.local>
2017-03-03 09:51:29 -0600

    add selector working

--- public/scripts/CustomReport.Controller.js
+++ public/scripts/CustomReport.Controller.js
@@ -4,5 +4,43 @@ angular.module("AngelApp").controller("CustomReportController", ['$location','$h
 
     var vm=this;
 
-
+    vm.dataSetList=[
+      {
+        title:'gender',
+        options:['male','female']
+      },
+      {
+        title:'age',
+        options:['20+','30+','40+','50+','60+']
+      },
+      {
+        title:'income',
+        options:[40000,50000,60000,70000,80000]
+      },
+      {
+        title:'marital status',
+        options:['married','single']
+      },
+    ]
+    vm.dataSetSelections=[];
+    vm.addSelection=function(category,option){
+      console.log('category',category);
+      console.log('option',option);
+        function isMatch(element,index,array){
+          return element.title == category.title
+        }
+      if(vm.dataSetSelections.some(isMatch)){
+        for(var i=0;i<vm.dataSetSelections.length;i++){
+            if (vm.dataSetSelections[i].title==category.title){
+              vm.dataSetSelections[i].options.push(option);
+            }
+        }
+      }else{
+          vm.dataSetSelections.push(
+            {
+              title:category.title,
+              options:[option]
+            });
+        }
+    }
 }]);
