f8ca8f7ae6442a965c0e31bd9adc3e6735f1b6f7
ejnelson <erik@ejnelson.com>
2017-03-10 16:59:25 -0600

    query strings building

--- public/scripts/controllers/CustomReport.Controller.js
+++ public/scripts/controllers/CustomReport.Controller.js
@@ -19,16 +19,27 @@ angular.module("AngelApp").controller("CustomReportController", ['$location','$h
         options:[40000,50000,60000,70000,80000]
       },
       {
-        title:'marital status',
+        title:'marital_status',
         options:['married','single']
       },
     ];  // end of dataSetList object
-
+    vm.columnLimitList=[];
+    for(var i=0;i<vm.dataSetList.length;i++){
+      vm.columnLimitList[i]=vm.dataSetList[i].title;
+    };
+    console.log(vm.columnLimitList);
     // vm.dataSetListCategoryOrder = ['gender', 'age', 'income', 'marital'];
     // trying to order the categories on the html page.
 
     vm.dataSetSelections=[{title:'no selections'}];
-
+    vm.columnLimitSelections=['no selections'];
+    vm.addColumnLimitSelection=function(category){
+      if (vm.columnLimitSelections[0] == 'no selections'){
+        vm.columnLimitSelections=[];
+      }
+      vm.columnLimitSelections.push(category);
+      console.log(vm.columnLimitSelections);
+    }
 
     vm.addSelection = function(category,option) {
       if (vm.dataSetSelections[0].title == 'no selections'){
@@ -57,7 +68,7 @@ angular.module("AngelApp").controller("CustomReportController", ['$location','$h
               options:[option]
             });
       }
-      console.log(vm);
+      // console.log(vm);
     }; // close addSelection
 
 
@@ -83,6 +94,63 @@ angular.module("AngelApp").controller("CustomReportController", ['$location','$h
       if(vm.dataSetSelections[0]==undefined){
           vm.dataSetSelections=[{title:'no selections'}];
         }console.log(vm.dataSetSelections[0]);
+    }
+    vm.removeColumnSelection=function(category){
+      for(var i=0;i<vm.columnLimitSelections.length;i++){
+          if (vm.columnLimitSelections[i]==category){
+            vm.columnLimitSelections.splice(i,1);
+          }
+      }console.log(vm.columnLimitSelections[0]);
+      if(vm.columnLimitSelections[0]==undefined){
+          vm.columnLimitSelections=['no selections'];
+        }console.log(vm.columnLimitSelections[0]);
+    }
+
+
+    vm.saveReport=function(){
+      var reportString="SELECT ";
+      console.log(reportString);
+      if(vm.columnLimitSelections!="no selections"){
+        for (var i=0;i<vm.columnLimitSelections.length-1;i++){
+
+          reportString+=(vm.columnLimitSelections[i]+",");
+        };
+        reportString+=(vm.columnLimitSelections[vm.columnLimitSelections.length-1]+" ");
+        reportString+="FROM patient ";
+        console.log(reportString);
+      }else{
+        reportString="SELECT * FROM patient ";
+      }
+      console.log(reportString);
+      if(vm.dataSetSelections[0]=="no selections"){
+        console.log(reportString);
+      }else{
+        reportString+="WHERE ";
+        for(var j=0;j<vm.dataSetSelections.length-1;j++){
+
+          if(vm.dataSetSelections[j].title=="age"||vm.dataSetSelections[j].title=="income"||vm.dataSetSelections[j].title=="qualify amount"||vm.dataSetSelections[j].title=="exp date"||vm.dataSetSelections[j].title=="app date"){
+            //write code based on format of these things!!!!
+          }else{
+            for(var i=0;i<vm.dataSetSelections[j].options.length-1;i++){
+              reportString+="("+vm.dataSetSelections[j].title+"='"+vm.dataSetSelections[j].options[i]+"') OR "
+            }
+            reportString+="("+vm.dataSetSelections[j].title+"='"+vm.dataSetSelections[j].options[vm.dataSetSelections[j].options.length-1]+"')";
+          }
+          reportString+=" AND ";
+        };
+        console.log(reportString);
+        if(vm.dataSetSelections[vm.dataSetSelections.length-1].title=="age"||vm.dataSetSelections[vm.dataSetSelections.length-1].title=="income"||vm.dataSetSelections[vm.dataSetSelections.length-1].title=="qualify amount"||vm.dataSetSelections[vm.dataSetSelections.length-1].title=="exp date"||vm.dataSetSelections[vm.dataSetSelections.length-1].title=="app date"){
+          //write code based on format of these things!!!!
+        }else{
+          for(var i=0;i<vm.dataSetSelections[vm.dataSetSelections.length-1].options.length-1;i++){
+            reportString+="("+vm.dataSetSelections[vm.dataSetSelections.length-1].title+"='"+vm.dataSetSelections[vm.dataSetSelections.length-1].options[i]+"') OR "
+          }
+          reportString+="("+vm.dataSetSelections[vm.dataSetSelections.length-1].title+"='"+vm.dataSetSelections[vm.dataSetSelections.length-1].options[vm.dataSetSelections[vm.dataSetSelections.length-1].options.length-1]+"')";
+        }
+      }
+      console.log(reportString);
+
+
     }
 
 
