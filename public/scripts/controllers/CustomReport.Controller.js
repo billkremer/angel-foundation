b3a0d6f354f8e4afdae5fed0dd9fbb363734944f
Bill Kremer <bkrems12@gmail.com>
2017-03-14 10:24:55 -0500

    cleared a bug when no filters were added.

--- public/scripts/controllers/CustomReport.Controller.js
+++ public/scripts/controllers/CustomReport.Controller.js
@@ -1,87 +1,87 @@
 // angular.module("AngelApp").controller("CustomReportController", ['$location','$http',
 //   function($location,$http) {
 angular.module("AngelApp").controller("CustomReportController", ['subcategoryBucketService','$location','$http',
-  function(subcategoryBucketService, $location, $http) {
+function(subcategoryBucketService, $location, $http) {
 
 
-    console.log('custom controller loaded');
+  console.log('custom controller loaded');
 
-    var vm=this;
+  var vm=this;
 
 
-    vm.dataSetList=[
-      {
-        title:'gender',
-        options:['female','male']
-      },
-      {
-        title:'age',
-        options:['20+','30+','40+','50+','60+']
-      },
-      {
-        title:'income',
-        options:[40000,50000,60000,70000,80000]
-      },
-      {
-        title:'marital_status',
-        options:['married','single']
-      },
-    ];  // end of dataSetList object
-    vm.columnLimitList=[];
-    for(var i=0;i<vm.dataSetList.length;i++){
-      vm.columnLimitList[i]=vm.dataSetList[i].title;
-    };
-    console.log(vm.columnLimitList);
-    // vm.dataSetListCategoryOrder = ['gender', 'age', 'income', 'marital'];
-    // trying to order the categories on the html page.
+  vm.dataSetList=[
+    {
+      title:'gender',
+      options:['female','male']
+    },
+    {
+      title:'age',
+      options:['20+','30+','40+','50+','60+']
+    },
+    {
+      title:'income',
+      options:[40000,50000,60000,70000,80000]
+    },
+    {
+      title:'marital_status',
+      options:['married','single']
+    },
+  ];  // end of dataSetList object
+  vm.columnLimitList=[];
+  for(var i=0;i<vm.dataSetList.length;i++){
+    vm.columnLimitList[i]=vm.dataSetList[i].title;
+  };
+  console.log(vm.columnLimitList);
+  // vm.dataSetListCategoryOrder = ['gender', 'age', 'income', 'marital'];
+  // trying to order the categories on the html page.
 
-    vm.dataSetSelections=[{title:'no selections'}];
-    vm.columnLimitSelections=['no selections'];
-    vm.addColumnLimitSelection=function(category){
-      if (vm.columnLimitSelections[0] == 'no selections'){
-        vm.columnLimitSelections=[];
-      }
-      vm.columnLimitSelections.push(category);
-      console.log(vm.columnLimitSelections);
+  vm.dataSetSelections=[{title:'no selections'}];
+  vm.columnLimitSelections=['no selections'];
+  vm.addColumnLimitSelection=function(category){
+    if (vm.columnLimitSelections[0] == 'no selections'){
+      vm.columnLimitSelections=[];
+    }
+    vm.columnLimitSelections.push(category);
+    console.log(vm.columnLimitSelections);
+  }
+
+  vm.addSelection = function(category,option) {
+    if (vm.dataSetSelections[0].title == 'no selections'){
+      vm.dataSetSelections=[];
     }
 
-    vm.addSelection = function(category,option) {
-      if (vm.dataSetSelections[0].title == 'no selections'){
-        vm.dataSetSelections=[];
-      }
+    console.log('category', category);
+    console.log('option', option);
 
-      console.log('category', category);
-      console.log('option', option);
+    function isMatch(element,index,array){
+      return element.title == category.title;
+    }
 
-        function isMatch(element,index,array){
-          return element.title == category.title;
-        }
-
-      if (vm.dataSetSelections.some(isMatch)) {
-        for(var i=0; i < vm.dataSetSelections.length; i++) {
-            if (vm.dataSetSelections[i].title == category.title){
-              if (vm.dataSetSelections[i].options.indexOf(option) == -1) {
-                vm.dataSetSelections[i].options.push(option);
-              };
-            };
+    if (vm.dataSetSelections.some(isMatch)) {
+      for(var i=0; i < vm.dataSetSelections.length; i++) {
+        if (vm.dataSetSelections[i].title == category.title){
+          if (vm.dataSetSelections[i].options.indexOf(option) == -1) {
+            vm.dataSetSelections[i].options.push(option);
+          };
         };
-      } else {
-          vm.dataSetSelections.push(
-            {
-              title:category.title,
-              options:[option]
-            });
+      };
+    } else {
+      vm.dataSetSelections.push(
+        {
+          title:category.title,
+          options:[option]
+        });
       }
-       console.log(vm);
+      console.log(vm);
     }; // close addSelection
 
 
     vm.removeOptionSelection = function(category,option){
       for(var i=0;i<vm.dataSetSelections.length;i++){
-          if (vm.dataSetSelections[i].title == category.title){
-            var index = vm.dataSetSelections[i].options.indexOf(option);
-            vm.dataSetSelections[i].options.splice(index,1);
-          }
+        if (vm.dataSetSelections[i].title == category.title){
+          var index = vm.dataSetSelections[i].options.indexOf(option);
+          vm.dataSetSelections[i].options.splice(index,1);
+        }
       } console.log(vm.dataSetSelections[0]);
       if(vm.dataSetSelections[0] == undefined){
         vm.dataSetSelections=[{title:'no selections'}];
@@ -91,30 +91,30 @@ angular.module("AngelApp").controller("CustomReportController", ['subcategoryBuc
 
     vm.removeCategorySelection=function(category){
       for(var i=0;i<vm.dataSetSelections.length;i++){
-          if (vm.dataSetSelections[i].title==category.title){
-            vm.dataSetSelections.splice(i,1);
-          }
+        if (vm.dataSetSelections[i].title==category.title){
+          vm.dataSetSelections.splice(i,1);
+        }
       }console.log(vm.dataSetSelections[0]);
       if(vm.dataSetSelections[0]==undefined){
-          vm.dataSetSelections=[{title:'no selections'}];
-        }console.log(vm.dataSetSelections[0]);
+        vm.dataSetSelections=[{title:'no selections'}];
+      }console.log(vm.dataSetSelections[0]);
     }
     vm.removeColumnSelection=function(category){
       for(var i=0;i<vm.columnLimitSelections.length;i++){
-          if (vm.columnLimitSelections[i]==category){
-            vm.columnLimitSelections.splice(i,1);
-          }
+        if (vm.columnLimitSelections[i]==category){
+          vm.columnLimitSelections.splice(i,1);
+        }
       }console.log(vm.columnLimitSelections[0]);
       if(vm.columnLimitSelections[0]==undefined){
-          vm.columnLimitSelections=['no selections'];
-        }console.log(vm.columnLimitSelections[0]);
+        vm.columnLimitSelections=['no selections'];
+      }console.log(vm.columnLimitSelections[0]);
     }
 
 
     vm.saveReport=function(){
       var reportString="SELECT ";
       console.log(reportString);
-      if(vm.columnLimitSelections!="no selections"){
+      if (vm.columnLimitSelections!="no selections") {
         for (var i=0;i<vm.columnLimitSelections.length-1;i++) {
           if (vm.columnLimitSelections[i] == "age") {
             reportString += "age(date_of_birth),";
@@ -131,13 +131,13 @@ angular.module("AngelApp").controller("CustomReportController", ['subcategoryBuc
 
         reportString+="FROM patient ";
         console.log(reportString);
-      }else{
-        reportString="SELECT * FROM patient ";
+      } else {
+        reportString = "SELECT * FROM patient ";
       }
       console.log(reportString);
-      if(vm.dataSetSelections[0]=="no selections"){
+      if (vm.dataSetSelections[0].title == "no selections"){
         console.log(reportString);
-      }else{
+      } else {
         reportString+="WHERE ";
         for(var j=0; j<vm.dataSetSelections.length-1; j++){
 
@@ -174,4 +174,4 @@ angular.module("AngelApp").controller("CustomReportController", ['subcategoryBuc
     }
 
 
-}]);
+  }]);
