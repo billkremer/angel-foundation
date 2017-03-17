d2e5c69dd6d16a41f05ad934a1c89090f29326bd
Bill Kremer <bkrems12@gmail.com>
2017-03-17 09:45:44 -0500

    connected button save to standard reports button

--- routes/data.js
+++ routes/data.js
@@ -30,4 +30,27 @@ router.get("/", function(req, res) {
     });
 });
 
+router.post("/", function(req, res) {
+    pool.connect(function(err, client, done) {
+        if (err) {
+            console.log("Error connecting to DB", err);
+            res.sendStatus(500);
+            done();
+        } else {
+            client.query("INSERT INTO standard_reports (report_name, query) VALUES ($1, $2)", [req.body.reportName, req.body.reportQuery], function(err, result) {
+                done();
+                if (err) {
+                    console.log("Error querying DB", err);
+                    res.sendStatus(500);
+                } else {
+                    console.log("Got info from DB", result.rows);
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});
+
+
+
 module.exports = router;
