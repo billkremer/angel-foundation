fb5d0663bec6e5872cb141c3de8c1fc90b5d7333
Bill Kremer <bkrems12@gmail.com>
2017-03-18 21:38:05 -0500

    hid console logs, added delete route, and added order by for sql query.
    added code to turn on or off console logs
    
    added a delete route to delete standard reports that are no longer wanted
    
    added order by for single database table querys for more consistent responses when recurring queries are made.

--- routes/standardReports.js
+++ routes/standardReports.js
@@ -7,27 +7,53 @@ var config = {
     database: "angel_data_reporting"
 };
 
+var verbose = false; // hides console logs
+
 var pool = new pg.Pool(config);
 
 router.get("/", function(req,res){
   pool.connect(function(err, client, done) {
     if (err) {
-        console.log("Error connecting to DB", err);
+        if (verbose) console.log("Error connecting to DB", err);
         res.sendStatus(500);
         done();
     } else {
-        client.query("SELECT * FROM standard_reports;", function(err, result) {
+        client.query("SELECT * FROM standard_reports ORDER BY report_number;", function(err, result) {
             done();
             if (err) {
-                console.log("Error querying DB", err);
+                if (verbose) console.log("Error querying DB", err);
                 res.sendStatus(500);
             } else {
-                console.log("Got info from DB", result.rows);
+                if (verbose) console.log("Got info from DB", result.rows);
                 res.send(result.rows);
             }
         });
     }
   })
 });//end of get all standard reports
+
+
+router.delete("/delReport/:id", function(req, res) {
+  if (verbose) console.log("delReport - req.params ",req.params);
+    pool.connect(function(err, client, done) {
+        if (err) {
+            if (verbose) console.log("Error connecting to DB", err);
+            res.sendStatus(500);
+            done();
+        } else {
+            client.query("DELETE FROM standard_reports WHERE report_number = $1;", [req.params.id], function(err, result) {
+                done();
+                if (err) {
+                    if (verbose) console.log("Error querying DB", err);
+                    res.sendStatus(500);
+                } else {
+                    if (verbose) console.log("Got info from DB", result.rows);
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = router;
