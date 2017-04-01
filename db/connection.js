964d4f87727bcd2ddbab0364de5be4ee51226b3d
Bill Kremer <bkrems12@gmail.com>
2017-03-31 20:48:18 -0500

    modified call to dotenv.

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,10 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
 var pg = require('pg');
+
+
+
 var pool = new pg.Pool({
   user: process.env.PGUSER, //env var: PGUSER
   database: process.env.PGDATABASE, //env var: process.env.PGDATABASE
@@ -7,6 +13,7 @@ var pool = new pg.Pool({
   port: 5432, //env var: PGPORT
   max: 30, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  ssl: true,
 });
 
 module.exports = pool;
